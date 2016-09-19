1628469ee5340836c803b351308c018141390d7d
TJ Alston <tjalston82@yahoo.com>
2016-09-19 10:32:13 -0400

    header home link set up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,15 @@ class Footer extends Component {
   render () {
     return <div>
       <footer>
+      <div className='resources'>
         <h3>Resources</h3>
+      </div>
+      <div className='api'>
         <h3>API Links</h3>
+      </div>
+      <div className='credits'>
         <h3>Credits</h3>
+      </div>
       </footer>
       <div className='copyright'>
       &#169;2016 Tameka J. Alston
