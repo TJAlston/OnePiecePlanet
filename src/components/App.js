52f7eba9b31254a03fc65cab0d3bfce120476bb5
TJ Alston <tjalston82@yahoo.com>
2016-09-20 12:35:53 -0400

    title page fixed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router'
 class App extends Component {
 
   render () {
-    return <div cla>
+    return <div className='titllePage'>
       <Link className='title' to ='/Home'>
         <img className='oplogo' src='http://i.imgur.com/sm7UQom.png' /> <h4>PLANET</h4>
         <img className='powerup' src='http://38.media.tumblr.com/c5f1df280fa287943dad93af323d37ed/tumblr_nvs2d9mdfI1s31fzzo1_500.gif' />
