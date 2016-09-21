cbdc83abf23d75034fa8723bcc161b4efd444181
TJ Alston <tjalston82@yahoo.com>
2016-09-20 21:36:45 -0400

    scroll fixed

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -13,22 +13,22 @@ class Anime extends Component {
           <div className='videos'>
             <div className='video1'>
               EPISODE 756
-              <iframe width='500' height='288' src='http://www.hulu.com/embed.html?eid=btznonsuogmz0awhehnjia&partner=funimation&url=http%3A%2F%2Fonepieceofficial.com%2Fvideos.aspx' scrolling='no' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+              <iframe width='500' height='288' src='http://www.hulu.com/embed.html?eid=btznonsuogmz0awhehnjia&partner=funimation&url=http%3A%2F%2Fonepieceofficial.com%2Fvideos.aspx' scrolling='no' allowFullScreen></iframe>
               </div>
               <div className='video2'>
               EPISODE 755
-              <iframe width='500' height='288' src='http://www.hulu.com/embed.html?eid=ey7esovrjarvoelg1uwc0g&partner=funimation&url=http%3A%2F%2Fwww.onepieceofficial.com%2Fvideos.aspx%3F%26v%3D9022' scrolling='no' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+              <iframe width='500' height='288' src='http://www.hulu.com/embed.html?eid=ey7esovrjarvoelg1uwc0g&partner=funimation&url=http%3A%2F%2Fwww.onepieceofficial.com%2Fvideos.aspx%3F%26v%3D9022' scrolling='no' allowFullScreen></iframe>
               </div>
               <div className='video3'>
               EPISODE 754
-              <iframe width='500' height='288' src='http://www.hulu.com/embed.html?eid=ujocuthazsye2s-bqcqb1g&partner=funimation&url=http%3A%2F%2Fwww.onepieceofficial.com%2Fvideos.aspx%3F%26v%3D9021' scrolling='no' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+              <iframe width='500' height='288' src='http://www.hulu.com/embed.html?eid=ujocuthazsye2s-bqcqb1g&partner=funimation&url=http%3A%2F%2Fwww.onepieceofficial.com%2Fvideos.aspx%3F%26v%3D9021' scrolling='no' allowFullScreen></iframe>
               </div>
+            </div>
             <div className='chapterList'>
               <a href='http://www.hulu.com/one-piece'>
               <h5>Complete List of Episodes Here</h5>
               </a>
             </div>
-          </div>
         </main>
       <Footer />
     </div>
