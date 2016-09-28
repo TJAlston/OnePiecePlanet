5e9658a2283d058a286db2f47f7f4d25a067b2b2
TJ Alston <tjalston82@yahoo.com>
2016-09-28 10:28:19 -0400

    logo fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,11 @@ class App extends Component {
 
   render () {
     return <div className='titlePage'>
-      {/* <audio src='http://onepiecetracklist.com/Music/450.mp3' autoPlay /> */}
+      <audio src='http://onepiecetracklist.com/Music/450.mp3' autoPlay />
       <Link className='title' to ='/Home'>
-        <img className='oplogo' src='../images/onepiecelogo1.svg' />
-        {/* <h4 className='titleh4'>PLANET</h4> */}
+        <img className='oplogo' src='https://raw.githubusercontent.com/TJAlston/OnePiecePlanet/master/src/images/onepiecelogo1.png'/>
         <img className='powerup' src='http://38.media.tumblr.com/c5f1df280fa287943dad93af323d37ed/tumblr_nvs2d9mdfI1s31fzzo1_500.gif' />
+        {/* <h4 className='titleh4'>PLANET</h4> */}
       </Link>
     </div>
   }
