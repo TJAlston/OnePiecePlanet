bef035fc66b965647d5a240fe312899d324cee55
TJAlston <tjalston82@gmail.com>
2016-10-05 14:50:11 -0400

    updated the most recent manga

--- src/components/Manga.js
+++ src/components/Manga.js
@@ -9,18 +9,18 @@ class Manga extends Component {
           <h1>Manga</h1>
           <div className='mangaSection'>
             <div className='mangaRow1'>
+              {/* Chapter 842 */}
+              <a className='recent1' href='http://www.zingbox.me/manga/One%20Piece/8168/65273321/2/193505/chapter842' />
               {/* Chapter 841 */}
-              <a className='recent1' href='http://www.zingbox.me/manga/One%20Piece/8168/960689/2/192915/chapter841' />
+              <a className='recent2' href='http://www.zingbox.me/manga/One%20Piece/8168/960689/2/192915/chapter841' />
               {/* Chapter 840 */}
-              <a className='recent2' href='http://www.zingbox.me/manga/One%20Piece/8168/95045980/2/191961/chapter840' />
+              <a className='recent3' href='http://www.zingbox.me/manga/One%20Piece/8168/95045980/2/191961/chapter840' />
               {/* Chapter 839 */}
-              <a className='recent3' href='http://www.zingbox.me/storyRead/8168?chapterId=191338&flag=2&randomKey=89428835' />
+              <a className='recent4' href='http://www.zingbox.me/storyRead/8168?chapterId=191338&flag=2&randomKey=89428835' />
               {/* Chapter 838 */}
-              <a className='recent4' href='http://www.zingbox.me/storyRead/8168?chapterId=189583&flag=2&randomKey=41857309' />
+              <a className='recent5' href='http://www.zingbox.me/storyRead/8168?chapterId=189583&flag=2&randomKey=41857309' />
               {/* Chapter 837 */}
-              <a className='recent5' href='http://www.zingbox.me/storyRead/8168?chapterId=188874&flag=2&randomKey=88825099' />
-              {/* Chapter 836 */}
-              <a className='recent6' href='http://www.zingbox.me/storyRead/8168?chapterId=188053&flag=2&randomKey=7720666' />
+              <a className='recent6' href='http://www.zingbox.me/storyRead/8168?chapterId=188874&flag=2&randomKey=88825099' />
             </div>
           </div>
           <div className='chapterList2'>
