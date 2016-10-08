b417a55d82d7c5e0741097fd5d558173f2ef91fb
TJAlston <tjalston82@gmail.com>
2016-10-08 00:14:17 -0400

    changed anime until new link is found

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -10,7 +10,7 @@ class Anime extends Component {
           <div className='videos'>
             <div className='video1'>
             EPISODE 758
-            <iframe width='500' height='288' src='http://embed.genfb.com/699167056898007' scrolling='no' allowFullScreen></iframe>
+            <iframe width='500' height='288' src='http://embed.genfb.com/545099389013795' scrolling='no' allowFullScreen></iframe>
             </div>
               <div className='video2'>
               EPISODE 757
